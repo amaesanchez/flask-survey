bc91be89ce74904d2449913990d9c1a46227b57a
Arlaine Sanchez <amaesanchez@gmail.com>
2022-12-01 14:56:23 -0500

    add first question

--- app.py
+++ app.py
@@ -7,3 +7,32 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+
+responses = []
+QUESTIONS = survey.questions
+
+@app.get("/")
+def start_survey():
+    """ renders start of survey """
+    title = survey.title
+    instructions = survey.instructions
+
+    return render_template("survey_start.html", title=title,
+        instructions=instructions)
+
+@app.post("/begin")
+def go_to_question():
+
+    return redirect("/questions/0")
+
+
+@app.get("/questions/<int:number>")
+def get_question(number):
+    survey = QUESTIONS[number]
+    curr_question = survey.prompt
+    curr_choice = survey.choices
+
+
+    return render_template("question.html", question_prompt=curr_question,
+        question_choice=curr_choice)
